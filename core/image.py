a151af736a3abc67b2e74cb6162f1fafa0cb58e4
gholmes829 <g.holmes429@gmail.com>
2020-10-09 17:10:18 -0500

    refactoring classes and interface

--- core/image.py
+++ core/image.py
@@ -3,7 +3,8 @@
 """
 
 import numpy as np
-from matplotlib.image import imread
+import numpy.linalg as la
+from PIL import Image as IM
 import matplotlib.pyplot as plt
 import os
 
@@ -24,14 +25,81 @@ Notes:
 	- Compare with SVD (?)
 """
 
+class Image:
+	def __init__(self, fileName):
+		self.resourcePath = os.path.join(os.getcwd(), "images")
+		self.path = os.path.join(self.resourcePath, fileName)
+		print("\nLoading image data from " + self.path)
+		self._img = IM.open(self.path)
+		self.isRGB = self.mode == "RGB"
+		self.data = ImageData(self._img)
+		
+		print("\t- operation was successful")
+		print("\nDimensions of image: " + str(self.data.shape))		
+
+	def makeBW(self):
+		self._img = self.convert("L")
+		self.data = ImageData(self._img)
+
+	def compress(self, components):
+		print("\nCompressing with "+str(components)+" components...")
+		self.data._data = pca(self.data._data, components, self.isRGB)
+		self.data.toInt()
+		self._img = IM.fromarray(self.data)
+
+
+	def save(self, name):
+		path = os.path.join(self.resourcePath, name+".jpg")
+		self._img.save(path)
+
+	def __getattr__(self, key):
+		if key == "_img":
+			raise AttributeError()
+		return getattr(self._img, key)
+
 class ImageData:
 	
-	def __init__(self, fileName):
-		resourcePath = os.path.join(os.getcwd(), "images")
-		self.path = os.path.join(resourcePath, fileName)
-		print("Loading image data from " + self.path)
-		self.data = imread(self.path)
-		print("\t- operation was successful")
+	def __init__(self, image):
+		self._data = np.asarray(image)
+		self.isRGB = len(self.shape) == 3 and self.data.shape[2] == 3
 
-		print("\nDimensions of image: " + str(self.data.shape))
+	def toInt(self):
+		minimum = self.min()
+		maximum = self.max()
+		diff = maximum-minimum
+		self._data = ((self._data-minimum)/maximum) * 255
+		self._data = self._data.astype(np.uint8)
+	
+	def __getattr__(self, key):
+		if key == "_data":
+			raise AttributeError()
+		return getattr(self._data, key)
+
+def pca(data, components, isRGB=True):
+	rows = data.shape[0]		
+	cols = data.shape[1]
+
+	if isRGB:
+		pass
+
+	mean = data.mean(axis=0)  # mean of each column
+	std = data.std(axis=0)  # standard deviation of each column
+	centered = data-mean  # centered at origin
+	adjusted = centered/std  # variance 1 across each axis
+	covariance = np.dot(adjusted.T, adjusted)/rows  # X.T*X/(n-1) 
+	eigVal, eigVec = la.eig(covariance)  # find unit eigen vectors and eigen values of covariance matrix
+	order = eigVal.argsort()[::-1]
+	eigVal = eigVal[order]
+	eigVec = eigVec[:,order]
+
+	feature = eigVec.copy()
+	
+	for i in range(1, feature.shape[1]-components + 1):
+		feature[:,feature.shape[1]-i] = np.zeros(cols).T
+	
+	projected = np.dot(feature.T, adjusted.T).T
+	normalized = la.multi_dot([feature, feature.T, adjusted.T]).T  # data projected onto principal subspace 
+	restored = normalized*std+mean
+	restored = np.absolute(restored).astype(np.float64)
+	return restored
 		
--- driver.py
+++ driver.py
@@ -2,12 +2,29 @@
 
 """
 
-from core.image import ImageData
+from core.image import Image
 
 class Driver:
 
 	def __init__(self):
-		pass
+		print("Image compressor...")
 
 	def run(self, fileName):
-		image = ImageData(fileName)
+		
+		image = Image(fileName)
+		image.makeBW()
+		image.compress(50)
+		image.show()
+		
+		"""
+		toTest = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 175, 200, 300, 400, 800]
+		
+		for c in toTest:
+			image = Image(fileName)
+			image.makeBW()
+			image.compress(c)
+			image.save("test#"+str(c))
+			#image.show()
+		"""
+		
+
